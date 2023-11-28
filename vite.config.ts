d572f4abecd38b40dd8e2e1e938ff7b9b6d4c7cf
Nathanael Björkgård <nathanael.bjorkgard@billes.se>
2023-11-28 10:32:05 +0100

    Rename package

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path"
-import dts from "vite-plugin-dts"
-import { libInjectCss } from "vite-plugin-lib-inject-css"
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ include: "lib", insertTypesEntry:true }),
-    libInjectCss(),
-],
-build: {
+  plugins: [react(), dts({ include: 'lib', insertTypesEntry: true }), libInjectCss()],
+  build: {
     // do not copy the contents of the public folder to the dist folder
     copyPublicDir: false,
     lib: {
       // this is the file that exports our components
-      entry: resolve(__dirname, "lib/poff-components-new.ts"),
-      name: "Poff Components",
-      fileName: "poff-components-new",
+      entry: resolve(__dirname, 'lib/poff-components.ts'),
+      name: 'Poff Components',
+      fileName: 'poff-components',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
